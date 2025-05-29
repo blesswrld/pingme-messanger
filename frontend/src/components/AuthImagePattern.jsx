332e50b82b2848b3e3d21ae8428227d75cbdf0b6
blesswrld <roninfortnite71@gmail.com>
2025-05-29 22:54:21 +0300

    fixed a bug related to a hidden section AuthImagePattern

--- frontend/src/components/AuthImagePattern.jsx
+++ frontend/src/components/AuthImagePattern.jsx
@@ -1,6 +1,10 @@
-const AuthImagePattern = ({ title, subtitle }) => {
+import { useTranslation } from "react-i18next";
+
+const AuthImagePattern = ({ titleKey, subtitleKey }) => {
+    const { t } = useTranslation();
     return (
         <div className="hidden lg:flex items-center justify-center bg-base-200 p-12">
+            {" "}
             <div className="max-w-md text-center">
                 <div className="grid grid-cols-3 gap-3 mb-8">
                     {[...Array(9)].map((_, i) => (
@@ -12,8 +16,8 @@ const AuthImagePattern = ({ title, subtitle }) => {
                         />
                     ))}
                 </div>
-                <h2 className="text-2xl font-bold mb-4">{title}</h2>
-                <p className="text-base-content/60">{subtitle}</p>
+                <h2 className="text-2xl font-bold mb-4">{t(titleKey)}</h2>
+                <p className="text-base-content/60">{t(subtitleKey)}</p>
             </div>
         </div>
     );
