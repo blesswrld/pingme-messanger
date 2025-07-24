4226659754a006afaab13d46094f921b49ca97a6
blesswrld <roninfortnite71@gmail.com>
2025-07-24 16:48:56 +0300

    divide backend - frontend parts (for prod version)

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,7 +20,27 @@ const __dirname = path.resolve();
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+
+const allowedOrigins = [
+    "http://localhost:5173",
+    "https://pingme-messanger-lm5r.onrender.com",
+];
+
+app.use(
+    cors({
+        origin: function (origin, callback) {
+            // allow requests with no origin (like mobile apps or curl requests)
+            if (!origin) return callback(null, true);
+            if (allowedOrigins.indexOf(origin) === -1) {
+                const msg =
+                    "The CORS policy for this site does not allow access from the specified Origin.";
+                return callback(new Error(msg), false);
+            }
+            return callback(null, true);
+        },
+        credentials: true,
+    })
+);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
